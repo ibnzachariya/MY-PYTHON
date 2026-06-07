c2748bafd3a3774b68ec90997e3fc530e6ae0c7a
ibnzachariya <ibnzachariya@gmail.com>
2026-06-07 22:21:58 +0100

    Refactor email sending code with context manager

--- PythonProject3/PythonProject/PythonProject1/Day-32/main.py
+++ PythonProject3/PythonProject/PythonProject1/Day-32/main.py
@@ -1,7 +1,7 @@
 import smtplib
 
 my_email = "zaabdulfataio@gmail.com"
-password = "jhrxtbpiotpjlywb"
+password = "password"
 
 # connection = smtplib.SMTP("smtp.gmail.com", 587)
 # connection.starttls()
